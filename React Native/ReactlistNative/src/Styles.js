77e594842aa6f9931feafa3f4a5b74c4fac0b11b
Eduardo Costa <eduardo.costa@docente.senai.br>
2026-08-11 08:15:29 -0300

    Projeto ReactListNative

---
+++ React Native/ReactlistNative/src/Styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+// style do app
+export const Styles = StyleSheet.create({
+    safeArea: {
+        flex: 1,
+        backgroundColor: "#fff"
+    },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#1D2135",
+    borderWidth: 3,
+    borderStyle: "solid",
+    borderColor: "red",
+  },
+});
---
+++ React Native/ReactlistNative/src/components/frmCadTask/Styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+export const Styles = StyleSheet.create({
+    frmCadTask: {
+        width: "100%",
+        height: 30,
+        
+        borderWidth: 2,
+        borderStyle: "solid",
+        borderColor: "white",
+        backgroundColor: "#1D2135"
+    },
+
+    frmCadTitle: {
+        color : "white"
+    }
+})
