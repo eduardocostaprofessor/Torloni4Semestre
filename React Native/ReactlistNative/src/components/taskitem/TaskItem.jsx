05a7bed694aacebec55d0b97a94334ac0c89377f
Eduardo Costa <eduardo.costa@docente.senai.br>
2026-08-13 08:21:16 -0300

    Adiciona API Fake + Get

--- React Native/ReactlistNative/src/components/taskitem/TaskItem.jsx
+++ React Native/ReactlistNative/src/components/taskitem/TaskItem.jsx
@@ -1,11 +1,31 @@
-import { Text, View } from "react-native";
+import { Image, Text, TouchableOpacity, View } from "react-native";
 import { TaskItemStyle } from "./TaskItemStyle";
 
-
-export const TaskItem = () => {
+// recebe o objeto do item como prop
+export const TaskItem = ( { dados: {id, descricao} } ) => {
+    
     return (
         <View style={TaskItemStyle.cardBox}>
-            <Text style={TaskItemStyle.cardText}>Estudar React Native</Text>
+            <Text style={TaskItemStyle.cardText}>
+                {descricao} - {id}
+                </Text>
+            
+            <TouchableOpacity style={[
+                TaskItemStyle.cardButton, 
+                TaskItemStyle.cardButtonEditColor
+                ]}>
+                <Image
+                style={TaskItemStyle.cardButtonImage} 
+                source={require("../../../assets/edit.png")}
+                />
+            </TouchableOpacity>
+            
+            <TouchableOpacity style={[TaskItemStyle.cardButton, TaskItemStyle.cardButtonThashColor]}>
+                <Image
+                style={TaskItemStyle.cardButtonImage} 
+                source={require("../../../assets/trash.jpg")}
+                />
+            </TouchableOpacity>
         </View>
     )
 }
--- React Native/ReactlistNative/src/components/tasklist/TaskList.jsx
+++ React Native/ReactlistNative/src/components/tasklist/TaskList.jsx
@@ -1,12 +1,62 @@
 import { ScrollView, Text } from "react-native";
 import { TaskListStyle } from "./TaskListStyle";
 import { TaskItem } from "../taskitem/TaskItem";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 export const TaskList = () => {
+
+  const [listaTarefas, setListaTarefas] = useState([])
+
+  // criar as funções 
+    const getTasks = async () => {
+      try {
+        // endereço do servidor (protocolo://endereçoDoServidor:porta/endpoint)
+        const APIReturn = await axios.get("http://172.16.1.105:3000/taskpoint")
+        const APIData = await APIReturn.data
+        
+        setListaTarefas(APIData)
+
+      } catch (error) {
+        console.log("Deu ruim na chamada da api");
+        console.log(error);
+        
+        
+      }
+      
+    }
+    // cadTask
+    const cadTask = async () => {
+      
+      console.log("FUNÇÃO POST EM DESENVOLVIMENTO");
+      
+    }
+    // putTask
+    const putTask = () => {
+      console.log("FUNÇÃO PUT EM DESENVOLVIMENTO");
+      
+    }
+    // deleteTask
+    const deleteTask = () => {
+      console.log("FUNÇÃO DELETE EM DESENVOLVIMENTO");
+      
+    }
+
+
+    // chama a listagem de tarefas na montagem do componente
+    useEffect(()=>{
+      getTasks()
+    }, [])
   return (
     <ScrollView style={TaskListStyle.taskListContainer}>
-      <TaskItem />
-      <TaskItem />
+      {
+        listaTarefas.map((tarefa)=> {
+          return(
+
+            <TaskItem dados={tarefa}/>
+          )
+        })
+      }
     </ScrollView>
   );
 };
